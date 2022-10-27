d6c557c94ed9c5c746099dcc1fbf27061d99ebc1
Yago Castro <yago.fanstour@gmail.com>
2022-10-27 17:16:41 -0300

    entrega requisito 5

--- src/App.js
+++ src/App.js
@@ -15,9 +15,36 @@ class App extends React.Component {
   };
 
   onInputChange = (event) => {
+    const { name, type, checked } = event.target;
+    const value = type === 'ceckbox' ? checked : event.target.value;
     this.setState({
-      [event.target.name]: event.target.value,
+      [name]: value,
     });
+  };
+
+  isSaveButtonDisabled = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+    const maxValueAtribute = 210;
+    const maxValue = 90;
+    const name = cardName.length > 0;
+    const descript = cardDescription.length > 0;
+    const image = cardImage.length > 0;
+    const rarity = cardRare.length > 0;
+    const atribts = parseInt(cardAttr1, 10)
+    + parseInt(cardAttr2, 10)
+    + parseInt(cardAttr3, 10) <= maxValueAtribute;
+    const atrib1 = parseInt(cardAttr1, 10) <= maxValue && parseInt(cardAttr1, 10) >= 0;
+    const atrib2 = parseInt(cardAttr2, 10) <= maxValue && parseInt(cardAttr2, 10) >= 0;
+    const atrib3 = parseInt(cardAttr3, 10) <= maxValue && parseInt(cardAttr3, 10) >= 0;
+    return name && descript && image && rarity && atribts && atrib1 && atrib2 && atrib3;
   };
 
   render() {
@@ -29,7 +56,8 @@ class App extends React.Component {
       cardAttr3,
       cardRare,
       cardTrunfo,
-      cardImage } = this.state;
+      cardImage,
+    } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
@@ -43,6 +71,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           src={ cardImage }
+          isSaveButtonDisabled={ !this.isSaveButtonDisabled() }
         />
         <Card
           cardName={ cardName }
